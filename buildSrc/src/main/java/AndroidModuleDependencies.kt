d15fb45f7556c971bb70c139bcdb07ebc2957502
hakzzang <mbh930312@naver.com>
2020-09-18 00:53:56 +0900

    gradle에서 사용하는 Dependencies 셋팅

---
+++ buildSrc/src/main/java/AndroidModuleDependencies.kt
@@ -0,0 +1,9 @@
+object AndroidModuleDependencies {
+    const val APPLICATION_ID = "com.hbs.myapplication"
+    const val TARGET_SDK_VERSION = 30
+    const val MIN_SDK_VERSION = 21
+    const val TOOL_VERSION = "30.0.2"
+
+    const val VERSION_CODE = 1
+    const val VERSION_NAME = "1.0"
+}
